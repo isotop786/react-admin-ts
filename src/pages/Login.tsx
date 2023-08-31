95b2eaf64b97dbcafc5d925dd7acb5d1629b37da
isotop786 <marufislam77@gmail.com>
2023-09-01 01:59:40 +0600

    login and registration improved

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { useFormik } from 'formik';
 import '../Login.css'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 const Login = () => {
+
+    const [isError,setError] = useState(false)
+    const [errMsg, setErrMsg] = useState('');
+    const [isSuccess, setSuccess] = useState(false);
+    const [isSubmitted, setSubmitted] = useState(false);
+
   const formik = useFormik({
     initialValues: {
       email: '',
@@ -13,6 +20,7 @@ const Login = () => {
     onSubmit: values => {
         if(values.email !=='' && values.password !=='')
         {
+            setSubmitted(true)
             // alert(JSON.stringify(values, null, 2));
             axios.post('https://nestadmin.onrender.com/api/login',{
                 email: values.email,
@@ -21,10 +29,22 @@ const Login = () => {
             .then((res)=>{
                 alert('Login success')
                 localStorage.setItem('auth_token',res.data.token)
+                setSubmitted(false)
             })
             .catch(err=>{
                 alert(err.response.data.message)
+                console.log(err.message)
+                console.log(err.response.data.message)
+                setError(true);
+                setErrMsg(err.response.data.message)
+                setSubmitted(false)
+               
+                // alert(err.response.data.message)
             })
+
+            setError(false)
+            setErrMsg('')
+           
         }
         else{
             alert('Email and passwor can not be empty')
@@ -34,8 +54,17 @@ const Login = () => {
   return (
     <div className="container">
         <div className="row">
-            <div className="col-md-12">
+            <div className="col-md-12" style={{maxWidth:"1100px"}}>
             <form className="form-signin" onSubmit={formik.handleSubmit}>
+            <h1 className="h3 mb-3 font-weight-normal">Please Login</h1>
+            {isError && (
+                        <div className="">
+                        <div className="alert alert-danger alert-dismissible fade show" role="alert">
+                            <strong>Error!</strong> {errMsg}.
+                            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    </div>
+                    )}
                 <label htmlFor="email" className="sr-only">Email Address</label>
                 <input
                     id="email"
@@ -56,7 +85,17 @@ const Login = () => {
                     className="form-control" placeholder="Password"
                 />
 
-                <button className='btn btn-primary my-4' type="submit">Submit</button>
+                
+                {!isSubmitted ?(
+                        <button className='btn btn-primary my-4' type="submit">Submit</button>
+                    ) :(
+                        <button className="btn btn-primary mt-3 " type="button" disabled>
+                            <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                            <span style={{marginLeft:".5rem"}} role="status">Login...</span>
+                        </button>
+                    )}
+                <p className="mt-5 mb-3 text-muted">Don't have account? <Link to="/register">Register</Link> here</p>
+                <p className="mt-5 mb-3 text-muted">&copy; MERN Admin Dashboard</p>
             </form>
             </div>
         </div>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,12 +3,14 @@ import React,{useEffect,useState} from 'react';
 import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import '../Login.css'
+// import '../Login.css'
 
 const Register = () => {
   
     const [isError,setError] = useState(false)
     const [errMsg, setErrMsg] = useState('');
+    const [isSuccess, setSuccess] = useState(false);
+    const [isSubmitted, setSubmitted] = useState(false);
   const formik = useFormik({
     initialValues: {
       firstName: '',
@@ -21,6 +23,7 @@ const Register = () => {
     onSubmit: values => {
         if(values.password === values.password_confirm)
         {
+            setSubmitted(true)
             // alert(JSON.stringify(values, null, 2));
             axios.post('https://nestadmin.onrender.com/api/register',{
                 first_name: values.firstName,
@@ -31,6 +34,7 @@ const Register = () => {
             })
             .then((res)=>{
                 alert('register success!')
+                setSuccess(true)
                 console.log(res.data)
             })
             .catch(err=>{
@@ -49,75 +53,101 @@ const Register = () => {
   return (
     <div className="container">
 
-        <div className="row">
-            
-            <div className="col-md-12">
-            <form className="form-signin" onSubmit={formik.handleSubmit}>
-            <h1 className="h3 mb-3 font-weight-normal">Registration Form</h1>
-                {isError && (
-                    <div className="">
-                    <div className="alert alert-warning alert-dismissible fade show" role="alert">
-                        <strong>Error!</strong> {errMsg}.
-                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <>
+            {!isSuccess ? (
+                <div className='row'>
+                <div className="col-md-12">
+                <form className="form-signin" onSubmit={formik.handleSubmit}>
+                <h1 className="h3 mb-3 font-weight-normal">Registration Form</h1>
+                    {isError && (
+                        <div className="">
+                        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>Error!</strong> {errMsg}.
+                            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
                     </div>
+                    )}
+                  
+                    <label htmlFor="firstName" className="sr-only">First Name</label>
+                    <input
+                        id="firstName"
+                        name="firstName"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.firstName}
+                        className="form-control" placeholder="First Name" required
+                    />
+                    
+    
+                    <label htmlFor="lastName" className="sr-only">Last Name</label>
+                    <input
+                        id="lastName"
+                        name="lastName"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.lastName}
+                        className="form-control" placeholder="Last Name" required
+                    />
+    
+                    <label htmlFor="email" className="sr-only">Email Address</label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                        className="form-control" placeholder="Email" required
+                    />
+    
+                    <label htmlFor="email" className="sr-only">Password</label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        className="form-control" placeholder="Password" required
+                    />
+    
+                    <label htmlFor="email" className="sr-only">Confirm Password</label>
+                    <input
+                        id="password_confirm"
+                        name="password_confirm"
+                        type="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password_confirm}
+                        className="form-control" placeholder="Confirm Password" required
+                    />
+                    {!isSubmitted ?(
+                        <button className='btn btn-primary mt-2' type="submit">Submit</button>
+                    ) :(
+                        <button className="btn btn-primary mt-3 " type="button" disabled>
+                            <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                            <span style={{marginLeft:".5rem"}} role="status">Registering...</span>
+                        </button>
+                    )}
+                    
+                    <p className="mt-5 mb-3 text-muted">Already have account? <Link to="/login">Login</Link> here</p>
+                    <p className="mt-5 mb-3 text-muted">&copy; MERN Admin Dashboard</p>
+                    </form>
                 </div>
-                )}
-                <label htmlFor="firstName" className="sr-only">First Name</label>
-                <input
-                    id="firstName"
-                    name="firstName"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.firstName}
-                    className="form-control" placeholder="First Name" required
-                />
-
-                <label htmlFor="lastName" className="sr-only">Last Name</label>
-                <input
-                    id="lastName"
-                    name="lastName"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.lastName}
-                    className="form-control" placeholder="Last Name" required
-                />
-
-                <label htmlFor="email" className="sr-only">Email Address</label>
-                <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                    className="form-control" placeholder="Email" required
-                />
-
-                <label htmlFor="email" className="sr-only">Password</label>
-                <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                    className="form-control" placeholder="Password" required
-                />
-
-                <label htmlFor="email" className="sr-only">Confirm Password</label>
-                <input
-                    id="password_confirm"
-                    name="password_confirm"
-                    type="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password_confirm}
-                    className="form-control" placeholder="Confirm Password" required
-                />
-
-                <button className='btn btn-primary mt-2' type="submit">Submit</button>
-                <p className="mt-5 mb-3 text-muted">Already have account? <Link to="/login">Login</Link> here</p>
-                <p className="mt-5 mb-3 text-muted">&copy; MERN Admin Dashboard</p>
-                </form>
-            </div>
-        </div>
+                </div>
+            ) : (
+                <div className='row justify-content-center pt-4 mt-4'>
+                    <div className='col-md-3'></div>
+                        <div className='col-md-6'>
+                            <div className="card card-default p-3">
+                                <div className="card-body text-center">
+                                    <h2 className='text-success'>Registration Success </h2>
+                                    <p><Link to={"/login"}>Login</Link> here</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-md-3'></div>
+                    </div>
+            )}
+            
+        </>
     </div>
   );
 };
