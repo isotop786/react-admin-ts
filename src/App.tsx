1f195eba1369b0c590f19b4da77fc8682904788a
isotop786 <marufislam77@gmail.com>
2023-08-31 13:59:35 +0600

    router init

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 // import './App.css';
 import { Nav } from './components/Nav';
 import Menu from './components/Menu';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Users from './pages/Users';
 
 function App() {
   return (
     <div>
-  
+  <BrowserRouter>
   <Nav/>
 
 <div className="container-fluid">
@@ -14,12 +17,18 @@ function App() {
 
       <Menu/>
 
-    <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+    <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4 ">
       
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/users' element={<Users/>}/>
+        </Routes>
+     
   
     </main>
   </div>
 </div>
+</BrowserRouter>
     </div>
   );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Menu:React.FC = () => {
   return (
@@ -6,31 +7,19 @@ const Menu:React.FC = () => {
       <div className="sidebar-sticky pt-3">
         <ul className="nav flex-column">
           <li className="nav-item">
-            <a className="nav-link active" href="#">
-              <span data-feather="home"></span>
-              Dashboard <span className="sr-only">(current)</span>
-            </a>
+            <Link to={"/"}  className="nav-link active">
+                Dashboard <span className="sr-only">(current)</span>
+            </Link>
           </li>
           
          
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <Link to={'/users'} className="nav-link" >
               <span data-feather="users"></span>
               Customers
-            </a>
+            </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <span data-feather="bar-chart-2"></span>
-              Reports
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Integrations
-            </a>
-          </li>
+      
         </ul>
 
       </div>
