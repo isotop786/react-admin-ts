aae3caf37be400a459d3da7d9f8ef2ada61f9dc8
isotop786 <marufislam77@gmail.com>
2023-09-03 22:06:04 +0600

    site updated

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -45,7 +45,7 @@ import { useNavigate } from "react-router-dom"
 
         return(
             <nav style={{height:"3.5rem"}} className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-                <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">MERN ADMIN</a>
+                <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">MERN CRM</a>
                 <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
